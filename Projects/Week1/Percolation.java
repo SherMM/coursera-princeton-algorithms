d446c6ff07e42e3731ba938734afaea60e0d1f98
Shermm <helioblast86@gmail.com>
2016-01-22 16:02:56 -0600

    first commit

---
+++ Projects/Week1/Percolation.java
@@ -0,0 +1,29 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+
+public class Percolation {
+
+public Percolation(int N) {
+
+}
+
+public void open(int i, int j) {
+
+}
+
+public boolean isOpen(int i, int j) {
+
+}
+
+public boolean isFull(int i, int j) {
+
+}
+
+public boolean percolates() {
+
+}
+
+public static void main(String[] args) {
+        System.out.println("Hello, World");
+}
+}
