e6455a2b2cb93760841dd1d7bfb2fb20f1a426b0
Ian <helioblast86@gmail.com>
2016-02-20 13:22:06 -0600

    updates

--- Projects/Week4/Solver.java
+++ Projects/Week4/Solver.java
@@ -1,19 +1,35 @@
 import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.Stack;
+import java.util.Comparator;
 
 public class Solver {
 
 private MinPQ<Board> pq;
 private int moves;
+private Comparator<Board> c;
 
 public Solver(Board initial) {
-        pq = new MinPQ<Board>();
+        moves = 0;
+        c = new Comparator<Board>() {
+                public int compare(Board b1, Board b2) {
+                        int b1Score = b1.hamming() + moves;
+                        int b2Score = b2.hamming() + moves;
+                        if (b1Score < b2Score) {
+                          return -1;
+                        } else if (b1Score > b2Score) {
+                          return +1;
+                        } else {
+                          return 0;
+                        }
+                }
+        };
+        pq = new MinPQ<Board>(c);
         moves = 0;
         pq.insert(initial);
 }
 
 public boolean isSolvable() {
-       return false;
+        return false;
 }
 
 public int moves() {
@@ -21,8 +37,8 @@ public int moves() {
 }
 
 public Iterable<Board> solution() {
-      Stack<Board> stack = new Stack<Board>();
-      return stack;
+        Stack<Board> stack = new Stack<Board>();
+        return stack;
 }
 
 public static void main(String[] args) {
