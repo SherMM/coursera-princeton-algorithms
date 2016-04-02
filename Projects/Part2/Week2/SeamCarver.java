fa9b45e021298f1258786b80bf6b1dba7454cf17
Shermm <helioblast86@gmail.com>
2016-04-02 12:32:15 -0500

    finished energy function

--- Projects/Part2/Week2/SeamCarver.java
+++ Projects/Part2/Week2/SeamCarver.java
@@ -1,3 +1,7 @@
+import java.awt.Color;
+import edu.princeton.cs.algs4.Picture;
+import edu.princeton.cs.algs4.StdOut;
+
 public class SeamCarver {
 
 private Picture seamPic;
@@ -10,33 +14,95 @@ public SeamCarver(Picture picture) {
 
 // current picture
 public Picture picture() {
-      return seamPic;
+        return seamPic;
 }
 
 // width of current picture
 public int width() {
-      return seamPic.width();
+        return seamPic.width();
 }
 
 // height of current picture
 public int height() {
-      return seamPic.height();
+        return seamPic.height();
 }
 
 // energy of pixel at column x and row y
 public double energy(int x, int y) {
+        if (isOnBorder(x, y, this.height(), this.width())) {
+          return 1000.0;
+        }
 
+        double xgrad = this.xGradient(x, y);
+        double ygrad = this.yGradient(x, y);
+
+        return Math.sqrt(xgrad + ygrad);
 }
 
-// sequence of indices for horizontal seam
-public int[] findHorizontalSeam() {
+private double xGradient(int x, int y) {
+        // get values at x+1, y pixel
+        Color colorHi = seamPic.get(x+1, y);
+        int rh = colorHi.getRed();
+        int gh = colorHi.getGreen();
+        int bh = colorHi.getBlue();
 
+        // get values at x-1, y pixel
+        Color colorLo = seamPic.get(x-1, y);
+        int rl = colorLo.getRed();
+        int gl = colorLo.getGreen();
+        int bl = colorLo.getBlue();
+
+        // get differences, squared
+        double rsq = Math.pow((double) rh - rl, 2.0);
+        double gsq = Math.pow((double) gh - gl, 2.0);
+        double bsq = Math.pow((double) bh - bl, 2.0);
+
+        double sqSum = rsq + gsq + bsq;
+
+        return sqSum;
 }
 
-// sequence of indices for vertical seam
-public int[] findVerticalSeam() {
+private double yGradient(int x, int y) {
+        // get values at x+1, y pixel
+        Color colorHi = seamPic.get(x, y+1);
+        int rh = colorHi.getRed();
+        int gh = colorHi.getGreen();
+        int bh = colorHi.getBlue();
 
+        // get values at x-1, y pixel
+        Color colorLo = seamPic.get(x, y-1);
+        int rl = colorLo.getRed();
+        int gl = colorLo.getGreen();
+        int bl = colorLo.getBlue();
+
+        // get differences, squared
+        double rsq = Math.pow((double) rh - rl, 2);
+        double gsq = Math.pow((double) gh - gl, 2);
+        double bsq = Math.pow((double) bh - bl, 2);
+
+        double sqSum = rsq + gsq + bsq;
+
+        return sqSum;
 }
+
+private static boolean isOnBorder(int x, int y, int height, int width) {
+        if (x == 0 || y == 0 || x == width-1 || y == height-1) {
+                return true;
+        }
+        return false;
+}
+
+/*
+   // sequence of indices for horizontal seam
+   public int[] findHorizontalSeam() {
+
+   }
+
+   // sequence of indices for vertical seam
+   public int[] findVerticalSeam() {
+
+   }
+ */
 
 // remove horizontal seam from current picture
 public void removeHorizontalSeam(int[] seam) {
